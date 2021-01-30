e58e591c2162cbbee3f4928c1a7bf60b3339978d
Tim Zoet <ts.zoet@gmail.com>
2021-01-30 22:13:53 +0100

    Improved delete and select sections

--- snippets/select_create.cpp
+++ snippets/select_create.cpp
@@ -5,4 +5,4 @@ const auto col1 = table.col<1>();
 
 // Prepare object to select first 2 columns of all rows
 // where 2nd column is in the range 10-20.
-auto select = table.select<0, 1>(col1 >= 10 && col1 <= 20);
+auto select = table.select<0, 1>(col1 >= 10 && col1 <= 20, true);
